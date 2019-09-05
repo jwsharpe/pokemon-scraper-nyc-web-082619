3820a3a4ab0032dca3b4b78ac8dd9ab7a57c3922
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-05 18:30:44 -0400

    Done.

--- bin/sql_runner.rb
+++ bin/sql_runner.rb
@@ -4,16 +4,16 @@ class SQLRunner
   end
 
   def execute_schema_migration_sql
-    sql = File.read('db/schema_migration.sql')
+    sql = File.read("db/schema_migration.sql")
     execute_sql(sql)
   end
 
   def execute_create_hp_column
-    sql = File.read('db/alter_table_migration.sql')
+    sql = File.read("db/alter_table_migration.sql")
     execute_sql(sql)
   end
 
   def execute_sql(sql)
-     sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
+    sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
   end
 end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+  attr_accessor :name, :type
+  attr_reader :id, :db
+
+  def initialize(attrs)
+    @name = attrs[:name]
+    @type = attrs[:type]
+    @id = attrs[:id]
+    @db = attrs[:db]
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) values (?,?)", name, type)
+    id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    Pokemon.new(name: name, type: type, id: id, db: db)
+  end
+
+  def self.find(id, db)
+    attrs = db.execute("SELECT name, type FROM pokemon WHERE id=?", id).first
+    name = attrs[0]
+    type = attrs[1]
+    Pokemon.new(name: name, type: type, id: id, db: db)
+  end
 end
